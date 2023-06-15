65414210dc346e1e4dbbd5a1907d104d3f323624
RvFerrao <rvferrao@gmail.com>
2023-06-15 19:33:22 -0300

    config files changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "purple-light": "#5C16C5",
+        "purple-dark": "#2D0C5E",
+        "gray-light": "#646464",
+        "gray-medium": "#323232",
+        "orange-dark": "#D18000",
+      },
+      dropShadow: {
+        "movie-poster": "0px 4px 4px rgba(0, 0, 0, 0.25)",
+      },
+      boxShadow: {
+        profile: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+      },
+    },
   },
   plugins: [],
 };
